7db820b142dbaba5c590bc4eaa6d6decef6b83e7
Repz Lopez <repzlopez@gmail.com>
2021-11-01 01:52:00 +0800

    sparta

--- index.py
+++ index.py
@@ -1,18 +1,18 @@
 import discord
 import os
 
-client = discord.Client()
+# client = discord.Client()
 
-@client.event
-async def on_ready():
-    print("THIS. IS. SPARTA!!!")
+# @client.event
+# async def on_ready():
+#     print("THIS. IS. SPARTA!!!")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
     
-    if message.content.startswith(">sparta"):
-        await message.channel.send("ahoo ahoo ahoo!!!")
+#     if message.content.startswith(">sparta"):
+#         await message.channel.send("ahoo ahoo ahoo!!!")
 
 print(os.environ['TOKEN'])
