6df8502ad7d27d640d6b43ec3b46e3766b43015d
Repz Lopez <repzlopez@gmail.com>
2021-11-01 10:16:52 +0800

    sparta

--- index.py
+++ index.py
@@ -1,7 +1,20 @@
 import discord
 import os
 
-if os.getenv('GALAWAN'):
-    print('Looks like GitHub!')
-else:
-    print('Maybe running locally?')
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("THIS. IS. SPARTA!!!")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(">sparta"):
+        await message.channel.send("ahoo ahoo ahoo!!!")
+
+client.run(os.environ['GALAWAN'])
