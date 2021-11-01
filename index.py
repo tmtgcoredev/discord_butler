d3ee3209942707fdc7c07d61f9fff1f46f9e526c
Repz Lopez <repzlopez@gmail.com>
2021-11-01 10:57:55 +0800

    sparta

--- index.py
+++ index.py
@@ -1,7 +1,12 @@
 import discord
 import os
 
-if os.getenv('CI'):
-    print('Looks like GitHub!')
-else:
-    print('Maybe running locally?')
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='>')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+bot.run(os.getenv('CI'))
