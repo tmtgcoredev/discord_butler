09f2eb8d1d23f2f89d932e6724c04d3e7140d561
Repz Lopez <repzlopez@gmail.com>
2021-11-01 10:04:11 +0800

    galawan

--- index.py
+++ index.py
@@ -1,21 +1,7 @@
 import discord
 import os
 
-from discord.client import Client
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print("THIS. IS. SPARTA!!!")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith(">sparta"):
-        await message.channel.send("ahoo ahoo ahoo!!!")
-
-print(os.getenv('jobs'))
-# client.run(piso)
+if os.getenv('GALAWAN'):
+    print('Looks like GitHub!')
+else:
+    print('Maybe running locally?')
